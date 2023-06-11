b51b185c2f6a3318e2bbbfbec7c707fe79017d54
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-06-11 11:39:54 -0400

    commit tree 20bc0d4eb24a33eb11c234f28de2124493a413cf

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,22 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+require 'pry'
+
+# Build a class EmailAddressParser that accepts a string of unformatted emails.
+class EmailAddressParser
+    attr_accessor :emails
+
+  def initialize(emails=[])
+        @emails = emails 
+
+  end
+ # The parse method on the class should separate them into unique email addresses. 
+
+ def parse
+      # binding.pry
+  @emails.split(/, | /).uniq
+  
+ end
+#The delimiters to support are commas (',')
+# or whitespace (' '). /w(com,)/
+
+
+end
